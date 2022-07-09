099710406da7a4843b332c35bd6b6e4a45b30c54
zacjjohnson <wecooked@gmail.com>
2022-07-09 15:16:10 -0400

    initial files added

---
+++ Public/scripts.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    console.log("Scripts have been loaded!");
+};
+
+// window.addEventListener("load", () => {
+//     console.log("Scripts have been loaded yet again!!");
+// });
